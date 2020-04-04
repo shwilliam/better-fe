782b5e1ee33179c035c66023206c14811ad54956
William L <w-lindvall@outlook.com>
2020-04-03 22:02:41 -0700

    refactor code editor using `react-ace`

--- www/src/components/Editor.js
+++ www/src/components/Editor.js
@@ -1,6 +1,77 @@
-import React from 'react'
-import {Tabs, Tab, TextArea, CodeSnippet} from 'carbon-components-react'
+import React, {useState, useCallback} from 'react'
+import AceEditor from 'react-ace'
+import {Tabs, Tab, CodeSnippet, Dropdown} from 'carbon-components-react'
 import {noop} from '../utils'
+
+// code editor deps
+import 'ace-builds/src-min-noconflict/ext-language_tools'
+import 'ace-builds/src-min-noconflict/snippets/html'
+import 'ace-builds/src-min-noconflict/snippets/javascript'
+import 'ace-builds/src-min-noconflict/snippets/css'
+import 'ace-builds/src-noconflict/mode-html'
+import 'ace-builds/src-noconflict/mode-javascript'
+import 'ace-builds/src-noconflict/mode-css'
+import 'ace-builds/src-noconflict/theme-monokai'
+import 'ace-builds/src-noconflict/theme-github'
+import 'ace-builds/src-noconflict/theme-tomorrow'
+import 'ace-builds/src-noconflict/theme-kuroir'
+import 'ace-builds/src-noconflict/theme-xcode'
+import 'ace-builds/src-noconflict/theme-textmate'
+import 'ace-builds/src-noconflict/theme-solarized_dark'
+import 'ace-builds/src-noconflict/theme-solarized_light'
+import 'ace-builds/src-noconflict/theme-terminal'
+
+const THEMES = [
+  {
+    label: 'Monokai',
+    value: 'monokai',
+  },
+  {
+    label: 'GitHub',
+    value: 'github',
+  },
+  {
+    label: 'Tomorrow',
+    value: 'tomorrow',
+  },
+  {
+    label: 'Kuroir',
+    value: 'kuroir',
+  },
+  {
+    label: 'Xcode',
+    value: 'xcode',
+  },
+  {
+    label: 'Text mate',
+    value: 'textmate',
+  },
+  {
+    label: 'Solarized dark',
+    value: 'solarized_dark',
+  },
+  {
+    label: 'Solarized light',
+    value: 'solarized_light',
+  },
+  {
+    label: 'Terminal',
+    value: 'terminal',
+  },
+]
+
+const THEME_OPTIONS = {
+  fontSize: 15,
+  tabSize: 2,
+  maxLines: 15,
+  minLines: 15,
+  enableBasicAutocompletion: true,
+  enableLiveAutocompletion: true,
+  enableSnippets: true,
+  editorProps: {
+    $blockScrolling: Infinity,
+  },
+}
 
 export const Editor = ({
   html,
@@ -10,56 +81,77 @@ export const Editor = ({
   css,
   onCssChange = noop,
   readOnly = false,
-}) => (
-  <div className="editor__input-container">
-    <Tabs>
-      <Tab id="html-editor" label="HTML">
-        {readOnly ? (
-          <CodeSnippet type="multi">
-            {html || 'Nothing to see here...'}
-          </CodeSnippet>
-        ) : (
-          <TextArea
-            labelText="HTML editor"
-            placeholder="<p>Hello world</p>"
-            hideLabel={true}
-            value={html}
-            onChange={onHtmlChange}
-          />
-        )}
-      </Tab>
-      <Tab id="js-editor" label="JS">
-        {readOnly ? (
-          <CodeSnippet type="multi">
-            {js || 'Nothing to see here...'}
-          </CodeSnippet>
-        ) : (
-          <TextArea
-            labelText="JS editor"
-            placeholder="console.log('🚀')"
-            hideLabel={true}
-            value={js}
-            onChange={onJsChange}
-          />
-        )}
-      </Tab>
-      <Tab id="css-editor" label="CSS">
-        {readOnly ? (
-          <CodeSnippet type="multi">
-            {css || 'Nothing to see here...'}
-          </CodeSnippet>
-        ) : (
-          <TextArea
-            labelText="CSS editor"
-            placeholder={`a {
-  color: papayawhip;
-}`}
-            hideLabel={true}
-            value={css}
-            onChange={onCssChange}
-          />
-        )}
-      </Tab>
-    </Tabs>
-  </div>
-)
+}) => {
+  const [activeTheme, setActiveTheme] = useState(THEMES[2])
+  const handleChangeTheme = useCallback(selection => {
+    if (selection?.selectedItem) {
+      setActiveTheme(selection?.selectedItem)
+    }
+  }, [])
+
+  return (
+    <div className="editor__input-container">
+      {!readOnly && (
+        <Dropdown
+          className="editor__input-theme"
+          label="Theme"
+          id="editor__input-theme"
+          items={THEMES}
+          itemToString={item => item?.label}
+          onChange={handleChangeTheme}
+          initialSelectedItem={activeTheme}
+        />
+      )}
+      <Tabs>
+        <Tab id="html-editor" label="HTML">
+          {readOnly ? (
+            <CodeSnippet type="multi">
+              {html || 'Nothing to see here...'}
+            </CodeSnippet>
+          ) : (
+            <AceEditor
+              name="html-editor"
+              mode="html"
+              theme={activeTheme.value}
+              value={html}
+              onChange={onHtmlChange}
+              {...THEME_OPTIONS}
+            />
+          )}
+        </Tab>
+        <Tab id="js-editor" label="JS">
+          {readOnly ? (
+            <CodeSnippet type="multi">
+              {js || 'Nothing to see here...'}
+            </CodeSnippet>
+          ) : (
+            <AceEditor
+              name="js-editor"
+              mode="javascript"
+              theme={activeTheme.value}
+              value={js}
+              onChange={onJsChange}
+              {...THEME_OPTIONS}
+            />
+          )}
+        </Tab>
+        <Tab id="css-editor" label="CSS">
+          {readOnly ? (
+            <CodeSnippet type="multi">
+              {css || 'Nothing to see here...'}
+            </CodeSnippet>
+          ) : (
+            <AceEditor
+              name="css-editor"
+              mode="css"
+              theme={activeTheme.value}
+              value={css}
+              onChange={onCssChange}
+              {...THEME_OPTIONS}
+            />
+          )}
+        </Tab>
+      </Tabs>
+    </div>
+  )
+}
--- www/src/hooks/use-editor.js
+++ www/src/hooks/use-editor.js
@@ -1,9 +1,9 @@
-import {useInput} from './use-input'
+import {useState} from 'react'
 
 export const useEditor = boilerplate => {
-  const [html, handleHTMLChange] = useInput(boilerplate?.html || '')
-  const [js, handleJSChange] = useInput(boilerplate?.js || '')
-  const [css, handleCSSChange] = useInput(boilerplate?.css || '')
+  const [html, handleHTMLChange] = useState(boilerplate?.html || '')
+  const [js, handleJSChange] = useState(boilerplate?.js || '')
+  const [css, handleCSSChange] = useState(boilerplate?.css || '')
 
   return {
     html,
